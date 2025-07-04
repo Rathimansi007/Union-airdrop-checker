a8c451e416faf1bd7d40389973c28bf07d3d2c79
NJweb3 <rathimansi736@gmail.com>
2025-07-04 16:25:24 +0530

    server.js

--- server.js
+++ server.js
@@ -47,10 +47,9 @@ app.post("/api/check", async (req, res) => {
 });
 
 // Serve static HTML from /public
-app.use(express.static(path.join(__dirname, "public")));
-
+res.sendFile(path.join(__dirname, "index.html"));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join((__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
