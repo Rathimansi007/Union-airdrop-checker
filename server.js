5eecef03128eceda0455da90bf25c0ad44412e3e
NJweb3 <rathimansi736@gmail.com>
2025-07-04 18:05:35 +0530

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const path = require("path");
 const fetch = require("node-fetch");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
+
 
 app.use(express.json());
 
